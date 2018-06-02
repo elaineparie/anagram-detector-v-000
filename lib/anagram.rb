7699ef8013ec5a807f68aea643e30a64b4993879
Elaine Anderson <eparie@bu.edu>
2018-06-02 14:50:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ def initialize(word)
 end
 
 def match(words_array)
-  words_array.collect do |w|
+  words_array.select do |w|
     sorted_array = w.split("")
     sorted_word = @word.split("")
   #  sorted_array.collect do |x|
