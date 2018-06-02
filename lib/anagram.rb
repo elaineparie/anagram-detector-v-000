7aff20293ed0827cad50a9fdb24ba4983f7ab1e3
Elaine Anderson <eparie@bu.edu>
2018-06-02 14:43:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -16,10 +16,10 @@ def match(words_array)
   #  sorted_array.collect do |x|
  if sorted_array.sort == sorted_word.sort
    array = sorted_array.join
-
     else
     []
   end
+  array.delete_if{ |elem| elem == []}
 end
 end
 
