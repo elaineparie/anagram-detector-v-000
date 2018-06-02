ffa628b4152e35361ea1b5cb869daa007c1cb437
Elaine Anderson <eparie@bu.edu>
2018-06-02 13:14:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,10 +13,9 @@ def match(words_array)
   words_array.each do |w|
     sorted_array = w.split("").sort
     sorted_word = @word.split("").sort
-    binding.pry
     if
     sorted_array == sorted_word
-      sorted_word[sorted_word]
+      sorted_array
     else
       []
 end
