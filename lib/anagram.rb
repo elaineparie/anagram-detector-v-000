171a77a8daa124dce511dcd8646762c5878a9298
Elaine Anderson <eparie@bu.edu>
2018-06-02 14:02:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,6 +15,7 @@ def match(words_array)
     sorted_word = @word.split("").sort
     if sorted_array == sorted_word
       sorted_word.word = sorted_array
+      sorted_array
     else
     []
   end
