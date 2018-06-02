c6734b4940be3509f5f3fb75eeda4d62797ca661
Elaine Anderson <eparie@bu.edu>
2018-06-02 12:56:01 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(words_array)
+  words_array.each do |w|
+    a_l_array = w.split("").sort
+    sorted_word = @word.split("")
+    sorted_word.sort
+    if a_l_array == sorted_word
+      sorted_word
+end
+
+end
